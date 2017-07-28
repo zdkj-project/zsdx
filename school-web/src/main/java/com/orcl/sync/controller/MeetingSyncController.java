087bbc964c18cb7f383177994dd94d174e48883a
autumnal_wind <autumnal_wind@yeah.net>
2017-07-28 14:14:25 +0800

    1.增加了同步后处理会议人员功能 2.完善了前端同步时的提示

--- school-web/src/main/java/com/orcl/sync/controller/MeetingSyncController.java
+++ school-web/src/main/java/com/orcl/sync/controller/MeetingSyncController.java
@@ -28,8 +28,8 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/meetingsync")
-public class MeetingSyncController extends FrameWorkController<DocSendcheck> implements Constant{
-    @Resource  
+public class MeetingSyncController extends FrameWorkController<DocSendcheck> implements Constant {
+    @Resource
     //orcl的session工厂
     private SessionFactory sssssss;
     @Resource
@@ -40,113 +40,133 @@ public class MeetingSyncController extends FrameWorkController<DocSendcheck> imp
     private OaMeetingempService empService;
     @Resource
     private BuildRoominfoService buildService;
-    
-	@RequestMapping(value = "meeting")
-	public void meeting(HttpServletRequest request, HttpServletResponse response) throws IOException, ParseException {
-		//启用orcl数据库
-		CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_ORACLE);
-		
-		//3.打开session  
-	    Session session = sssssss.openSession();  
-	    //4.开启事务  
-	    session.beginTransaction();
-	
-	    Query query = session.createSQLQuery("select MEETING_ID,cast(MEETING_TITLE as VARCHAR2(255)),cast(CONTENT as VARCHAR2(2048))," +
-                "cast(MEETING_CATEGORY as VARCHAR2(255)),BEGIN_TIME,END_TIME,cast(ROOM_NAME as VARCHAR2(255)) from zsdx_sync.meeting_msg");
-	    //session.createSQLQuery("update dept set dname='SALES1' where deptno=30").executeUpdate();
-	    List<Object[]> list = query.list();
-	    
-	    //6.提交事务  
-	    session.getTransaction().commit();  
-	    //7.关闭session  
-	    session.flush(); 
-	    
-	    CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_MYSQL);
-	    OaMeeting m=null;
-	    //获取房间实体，拿到roomid
-	    BuildRoominfo build=null;
-	    for(Object[] o:list){
-    		m=meetingService.get(o[0].toString());
-    		if (m==null) {
-				m=new OaMeeting(o[0].toString());
-			}
-    		m.setMeetingTitle(o[1].toString());
-    		m.setMeetingName(o[1].toString());
-    		m.setMeetingContent(o[2].toString());
-    		m.setMeetingCategory(o[3].toString());
-    		
-    		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//小写的mm表示的是分钟    
-    		m.setBeginTime(sdf.parse(o[4].toString()));
-    		m.setEndTime(sdf.parse(o[5].toString()));
-    		//m.setRoomId(o[7].toString());
-    		m.setRoomName(o[6].toString());
-    		
-    		build=buildService.getByProerties("roomName", o[6].toString());
-    		if(ModelUtil.isNotNull(build)){
-    			m.setRoomId(build.getUuid());
-    		}
-    		
-    		m.setNeedChecking((short) 1);
-    		meetingService.merge(m);
-	    }
-	    employee();
 
-	    
-	    writeJSON(response, jsonBuilder.returnSuccessJson("'同步成功'"));
-	}
-	
-	public void employee() throws ParseException{
-		//启用orcl数据库
-		CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_ORACLE);
-		
-		//3.打开session  
-	    Session session = sssssss.openSession();  
-	    //4.开启事务  
-	    session.beginTransaction();
-	
-	    Query query = session.createSQLQuery("select MEETING_ID,EMPLOYEE_ID,cast(XM as VARCHAR2(255)) from zsdx_sync.meeting_user");
-	    //session.createSQLQuery("update dept set dname='SALES1' where deptno=30").executeUpdate();
-	    List<Object[]> list = query.list();
-	    
-	    //6.提交事务  
-	    session.getTransaction().commit();  
-	    //7.关闭session  
-	    session.flush(); 
-	    
-	    CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_MYSQL);
-	    OaMeetingemp m=null;
-	    for(Object[] o:list){
-    		m=empService.get(o[0].toString()+o[1].toString());
-    		if (m==null) {
-				m=new OaMeetingemp(o[0].toString()+o[1].toString());
-			}
-    		m.setMeetingId(o[0].toString());
-    		m.setEmployeeId(o[1].toString());
-    		m.setAttendResult("1");
-    		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//小写的mm表示的是分钟    
-    		Query query1 = session.createSQLQuery("select BEGIN_TIME,END_TIME from zsdx_sync.meeting_msg where MEETING_ID='"+o[0].toString()+"'");
-    		List<Object[]> list1 = query1.list();
-    		if(list1.size()>0){
-        		for(Object[] o1:list1){
-            		m.setBeginTime(sdf.parse(o1[0].toString()));
-            		m.setEndTime(sdf.parse(o1[1].toString()));
-        		}
-    		}else{
-        		m.setBeginTime(sdf.parse("1970-01-01 00:00:00"));
-        		m.setEndTime(sdf.parse("1970-01-01 00:00:00"));
-    		}
+    @RequestMapping(value = "meeting")
+    public void meeting(HttpServletRequest request, HttpServletResponse response) throws IOException, ParseException {
+        request.getSession().setAttribute("syncMeetingInfoIsEnd", "0");
+        request.getSession().removeAttribute("syncMeetingInfoState");
+        //启用orcl数据库
+        CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_ORACLE);
 
-    		m.setXm(o[2].toString());
-    		empService.merge(m);
+        //3.打开session
+        Session session = sssssss.openSession();
+        //4.开启事务
+        session.beginTransaction();
+
+        Query query = session.createSQLQuery("select MEETING_ID,cast(MEETING_TITLE as VARCHAR2(255)),cast(CONTENT as VARCHAR2(2048))," +
+                "cast(MEETING_CATEGORY as VARCHAR2(255)),BEGIN_TIME,END_TIME,cast(ROOM_NAME as VARCHAR2(255)) from zsdx_sync.meeting_msg ORDER BY  BEGIN_TIME ASC");
+        //session.createSQLQuery("update dept set dname='SALES1' where deptno=30").executeUpdate();
+        List<Object[]> list = query.list();
+
+        //6.提交事务
+        session.getTransaction().commit();
+        //7.关闭session
+        session.flush();
+
+        CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_MYSQL);
+        OaMeeting m = null;
+        //获取房间实体，拿到roomid
+        BuildRoominfo build = null;
+        for (Object[] o : list) {
+            m = meetingService.get(o[0].toString());
+            if (m == null) {
+                m = new OaMeeting(o[0].toString());
+            }
+            m.setMeetingTitle(o[1].toString());
+            m.setMeetingName(o[1].toString());
+            m.setMeetingContent(o[2].toString());
+            m.setMeetingCategory(o[3].toString());
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//小写的mm表示的是分钟
+            m.setBeginTime(sdf.parse(o[4].toString()));
+            m.setEndTime(sdf.parse(o[5].toString()));
+            //m.setRoomId(o[7].toString());
+            m.setRoomName(o[6].toString());
+
+            build = buildService.getByProerties("roomName", o[6].toString());
+            if (ModelUtil.isNotNull(build)) {
+                m.setRoomId(build.getUuid());
+            }
+
+            m.setNeedChecking((short) 1);
+            meetingService.merge(m);
+        }
+        employee();
+
+        request.getSession().setAttribute("syncMeetingInfoIsEnd", "1");
 
 
-	    }
+        //writeJSON(response, jsonBuilder.returnSuccessJson("'同步成功'"));
+    }
 
-	    
-	    //writeJSON(response, jsonBuilder.returnSuccessJson("'同步成功'"));
-	}
-	
-	
-	
+    public void employee() throws ParseException {
+        //启用orcl数据库
+        CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_ORACLE);
 
+        //3.打开session
+        Session session = sssssss.openSession();
+        //4.开启事务
+        session.beginTransaction();
+
+        Query query = session.createSQLQuery("select MEETING_ID,EMPLOYEE_ID,cast(XM as VARCHAR2(255)) from zsdx_sync.meeting_user");
+        //session.createSQLQuery("update dept set dname='SALES1' where deptno=30").executeUpdate();
+        List<Object[]> list = query.list();
+
+        //6.提交事务
+        session.getTransaction().commit();
+        //7.关闭session
+        session.flush();
+
+        CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_MYSQL);
+        OaMeetingemp m = null;
+        for (Object[] o : list) {
+            m = empService.get(o[0].toString() + o[1].toString());
+            if (m == null) {
+                m = new OaMeetingemp(o[0].toString() + o[1].toString());
+            }
+            m.setMeetingId(o[0].toString());
+            m.setEmployeeId(o[1].toString());
+            m.setAttendResult("1");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//小写的mm表示的是分钟
+            Query query1 = session.createSQLQuery("select BEGIN_TIME,END_TIME from zsdx_sync.meeting_msg where MEETING_ID='" + o[0].toString() + "'");
+            List<Object[]> list1 = query1.list();
+            if (list1.size() > 0) {
+                for (Object[] o1 : list1) {
+                    m.setBeginTime(sdf.parse(o1[0].toString()));
+                    m.setEndTime(sdf.parse(o1[1].toString()));
+                }
+            } else {
+                m.setBeginTime(sdf.parse("1970-01-01 00:00:00"));
+                m.setEndTime(sdf.parse("1970-01-01 00:00:00"));
+            }
+
+            m.setXm(o[2].toString());
+            empService.merge(m);
+
+
+        }
+
+        String sql = "UPDATE dbo.OA_T_MEETING SET METTING_EMPID=(SELECT dbo.OA_F_GETMEETINGEMPID(MEETING_ID)),\n" +
+                "\tMEETING_EMPNMAE=(SELECT dbo.OA_F_GETMEETINGEMPNAME(MEETING_ID))";
+        empService.executeSql(sql);
+        //writeJSON(response, jsonBuilder.returnSuccessJson("'同步成功'"));
+    }
+
+    @RequestMapping("/checkSyncEnd")
+    public void checkSyncEnd(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        Object isEnd = request.getSession().getAttribute("syncMeetingInfoIsEnd");
+        Object state = request.getSession().getAttribute("syncMeetingInfoState");
+        if (isEnd != null) {
+            if ("1".equals(isEnd.toString())) {
+                writeJSON(response, jsonBuilder.returnSuccessJson("\"会议同步完成！\""));
+            } else if (state != null && state.equals("0")) {
+                writeJSON(response, jsonBuilder.returnFailureJson("0"));
+            } else {
+                writeJSON(response, jsonBuilder.returnFailureJson("\"会议同步未完成！\""));
+            }
+        } else {
+            writeJSON(response, jsonBuilder.returnFailureJson("\"会议同步未完成！\""));
+        }
+    }
 }
--- school-web/src/main/webapp/static/core/app/oa/meeting/meetinginfo/controller/MainController.js
+++ school-web/src/main/webapp/static/core/app/oa/meeting/meetinginfo/controller/MainController.js
@@ -8,25 +8,70 @@ Ext.define("core.oa.meeting.meetinginfo.controller.MainController", {
         gridActionUtil: "core.util.GridActionUtil",
         dateUtil: 'core.util.DateUtil'
     },
-    init: function() {
+    init: function () {
     },
     control: {
         "panel[xtype=meetinginfo.maingrid] button[ref=sync]": {
-            beforeclick: function(btn) {
-            	 var resObj = this.ajax({
-                     url: "/meetingsync" + "/meeting"
-                 });
-                 if (resObj.success) {
-                	 this.msgbox("同步成功!");
-                	 btn.up("basegrid").getStore().load();
-                 }else{
-                	 //self.msgbox("同步成功!");
-                	 this.msgbox("同步失败！");
-                 }
-                 return false;
+            beforeclick: function (btn) {
+                var self = this;
+                var title = "确定从OA同步会议信息吗？";
+                Ext.Msg.confirm('提示', title, function (btn2, text) {
+                    if (btn2 == "yes") {
+                        Ext.Msg.wait('正在同步中,请稍后...', '温馨提示');
+                        var component = Ext.create('Ext.Component', {
+                            title: 'HelloWorld',
+                            width: 0,
+                            height: 0,
+                            hidden: true,
+                            html: '<iframe src="' + comm.get('baseUrl') + '/meetingsync/meeting"></iframe>',
+                            renderTo: Ext.getBody()
+                        });
+
+                        var time = function () {
+                            self.syncAjax({
+                                url: comm.get('baseUrl') + '/meetingsync/checkSyncEnd',
+                                timeout: 1000 * 60 * 30,        //半个小时
+                                //回调代码必须写在里面
+                                success: function (response) {
+                                    data = Ext.decode(Ext.valueFrom(response.responseText, '{}'));
+                                    if (data.success) {
+                                        Ext.Msg.hide();
+                                        self.msgbox(data.obj);
+                                        component.destroy();
+
+                                        btn.up("basegrid").getStore().loadPage(1);
+                                    } else {
+                                        setTimeout(function () {
+                                            time()
+                                        }, 1000);
+                                    }
+                                },
+                                failure: function (response) {
+                                    Ext.Msg.alert('请求失败', '错误信息：\n' + response.responseText);
+                                    component.destroy();
+                                    clearInterval(interval)
+                                }
+                            });
+                        }
+                        setTimeout(function () {
+                            time()
+                        }, 1000);    //延迟1秒执行
+                    }
+                });
+/*                var resObj = this.ajax({
+                    url: "/meetingsync" + "/meeting"
+                });
+                if (resObj.success) {
+                    this.msgbox("同步成功!");
+                    btn.up("basegrid").getStore().load();
+                } else {
+                    //self.msgbox("同步成功!");
+                    this.msgbox("同步失败！");
+                }*/
+                return false;
             }
         },
-        
+
         //导入
         "basegrid[xtype=meetinginfo.maingrid] button[ref=gridImport]": {
             beforeclick: function (btn) {
@@ -57,7 +102,7 @@ Ext.define("core.oa.meeting.meetinginfo.controller.MainController", {
                 return false;
             }
         },
-        
+
         /**
          * 导出
          */
@@ -104,7 +149,7 @@ Ext.define("core.oa.meeting.meetinginfo.controller.MainController", {
                                     success: function (resp, opts) {
                                         //task.delay(1200);
                                         Ext.Msg.hide();
-                                        Ext.Msg.alert("消息","导出数据成功");
+                                        Ext.Msg.alert("消息", "导出数据成功");
                                     },
                                     failure: function (resp, opts) {
                                         task.delay(1200);
@@ -119,7 +164,7 @@ Ext.define("core.oa.meeting.meetinginfo.controller.MainController", {
                                 Ext.Msg.alert('错误');
                             }
                         });
-                        
+
 //                        Ext.Ajax.request({
 //                            url: comm.get('baseUrl') + '/OaMeeting/checkExportEnd',
 //                            success: function (resp, opts) {
@@ -138,166 +183,160 @@ Ext.define("core.oa.meeting.meetinginfo.controller.MainController", {
                 return false;
             }
         },
-    	
-    
+
+        /**
+         * 增加按钮事件
+         */
         "basegrid button[ref=gridAdd_Tab]": {
-            beforeclick: function(btn) {
+            beforeclick: function (btn) {
                 console.log(btn);
                 //return false;
             }
         },
 
-        "basegrid button[ref=gridDetail_tab]": {
-            beforeclick: function(btn) {
-                console.log(btn);
-                //return false;
-            }
-        },
-
-        "basegrid button[ref=gridEdit_tab]": {
-            beforeclick: function(btn) {
-                console.log(btn);
-                //return false;
-            }
-        },
-
+        /**
+         * 操作列事件
+         */
         "basegrid  actioncolumn": {
-            editClick: function(data) {
+            editClick: function (data) {
                 console.log(data);
 
             },
-            detailClick_Tab: function(data) {
-                var baseGrid=data.view;
-                var record=data.record;
-                this.doDetail_Tab(null,"detail",baseGrid,record);
-                 return false;
+            detailClick_Tab: function (data) {
+                var baseGrid = data.view;
+                var record = data.record;
+                this.doDetail_Tab(null, "detail", baseGrid, record);
+                return false;
             },
-            deleteClick: function(data) {
+            deleteClick: function (data) {
                 console.log(data);
             },
         }
     },
 
 
-    //这里写detail方法   
-        doDetail_Tab:function(btn,cmd,grid,record){
-                var self=this;
-                var baseGrid;
-                var recordData;
+    /**
+     * 详细操作的处理
+     * @param btn
+     * @param cmd
+     * @param grid
+     * @param record
+     */
+    doDetail_Tab: function (btn, cmd, grid, record) {
+        var self = this;
+        var baseGrid;
+        var recordData;
 
-            if (btn) {
-                baseGrid=btn.up("basegrid");
-            }else{
-                baseGrid=grid;
-                recordData=record.getData();
-            }
+        if (btn) {
+            baseGrid = btn.up("basegrid");
+        } else {
+            baseGrid = grid;
+            recordData = record.getData();
+        }
 
-
-            var funCode=baseGrid.funCode;
-            var basePanel=baseGrid.up("basepanel[funCode="+funCode+"]");
-            var tabPanel=baseGrid.up("tabpanel[xtype=app-main]");
-
-                                                                                 
-            var funData=basePanel.funData;
-            var detCode=basePanel.detCode;
-            var detLayout=basePanel.detLayout;
-            var defaultObj=funData.defaultObj;
-
-                                                                                
-            var otherController=basePanel.otherController;
-            if (!otherController) 
-                otherController='';
-                                                                                
-            var insertObj=self.getDefaultValue(defaultObj);
-            var popFunData=Ext.apply(funData,{
-                  grid:baseGrid
-            });
+        var funCode = baseGrid.funCode;
+        var basePanel = baseGrid.up("basepanel[funCode=" + funCode + "]");
+        var tabPanel = baseGrid.up("tabpanel[xtype=app-main]");
 
 
-           var tabTitle=funData.tabConfig.addTitle;
-           var tabItemId=funCode+"_gridDetail";
-           var pkValue=null;
-           var operType=cmd;
-       switch(cmd){
+        var funData = basePanel.funData;
+        var detCode = basePanel.detCode;
+        var detLayout = basePanel.detLayout;
+        var defaultObj = funData.defaultObj;
+
+
+        var otherController = basePanel.otherController;
+        if (!otherController)
+            otherController = '';
+
+        var insertObj = self.getDefaultValue(defaultObj);
+        var popFunData = Ext.apply(funData, {
+            grid: baseGrid
+        });
+
+
+        var tabTitle = funData.tabConfig.addTitle;
+        var tabItemId = funCode + "_gridDetail";
+        var pkValue = null;
+        var operType = cmd;
+        switch (cmd) {
             case "detail":
                 if (btn) {
-                    var rescords=baseGrid.getSelectionModel().getSelection();
-                    if (rescords.length !=1) {
+                    var rescords = baseGrid.getSelectionModel().getSelection();
+                    if (rescords.length != 1) {
                         self.msgbox("请选择一条数据！");
                         return;
                     }
-                    recordData=rescords[0].getData();
+                    recordData = rescords[0].getData();
                 }
-                var pkName=funData.pkName;
-                pkValue=recordData[pkName];
+                var pkName = funData.pkName;
+                pkValue = recordData[pkName];
 
-                insertObj=recordData;
-                tabTitle=funData.tabConfig.detailTitle;
-                tabItemId=funCode+"_gridDetail"+insertObj.uuid;
-                operType="Detail";
+                insertObj = recordData;
+                tabTitle = funData.tabConfig.detailTitle;
+                tabItemId = funCode + "_gridDetail" + insertObj.uuid;
+                operType = "Detail";
                 break;
         }
 
 
-         var tabItem=tabPanel.getComponent(tabItemId);
-                if (!tabItem) {
-                    tabItem=Ext.create({
-                        xtype:'container',
-                        title:tabTitle,
-                        scrollable:true,
-                        itemId:tabItemId,
-                        itemPKV:pkValue,
-                        layout:'fit',
-                    });
-                    tabPanel.add(tabItem);
-            
-                              
-                    setTimeout(function(){
-                        var item=Ext.widget("baseformtab",{
-                            operType:operType,
-                            controller:otherController,
-                            funCode:funCode,
-                            detCode:detCode,
-                            tabItemId:tabItemId,
-                            insertObj:insertObj,
-                            funData:popFunData,
-                            items:[{
-                                xtype:detLayout,
-                                funCode:detCode,
-                                items: [{
-                                    xtype: "meetinginfo.DetailPanel"
-                                }]
-                            }]
-                        });
+        var tabItem = tabPanel.getComponent(tabItemId);
+        if (!tabItem) {
+            tabItem = Ext.create({
+                xtype: 'container',
+                title: tabTitle,
+                scrollable: true,
+                itemId: tabItemId,
+                itemPKV: pkValue,
+                layout: 'fit',
+            });
+            tabPanel.add(tabItem);
 
-                        tabItem.add(item);
-                        
 
-                        var detailhtmlpanel = item.down("container[xtype=meetinginfo.DetailPanel]");
+            setTimeout(function () {
+                var item = Ext.widget("baseformtab", {
+                    operType: operType,
+                    controller: otherController,
+                    funCode: funCode,
+                    detCode: detCode,
+                    tabItemId: tabItemId,
+                    insertObj: insertObj,
+                    funData: popFunData,
+                    items: [{
+                        xtype: detLayout,
+                        funCode: detCode,
+                        items: [{
+                            xtype: "meetinginfo.DetailPanel"
+                        }]
+                    }]
+                });
 
-                        //处理数据字段的值                                       
-                        var ddItem = factory.DDCache.getItemByDDCode("MEETINGCATEGORY");
-                        var resultVal="";
-                        var value=recordData["meetingCategory"];
-                        for (var i = 0; i < ddItem.length; i++) {
-                            var ddObj = ddItem[i];
-                            if (value == ddObj["itemCode"]) {
-                                resultVal = ddObj["itemName"];
-                                break;
-                            }                                                    
-                        }
-                        recordData.meetingCategory=resultVal;  
+                tabItem.add(item);
 
-                        recordData.needChecking = recordData.needChecking==1?"需要考勤":"不考勤";
 
-                        detailhtmlpanel.setData(recordData);                       
-                        
-                    },30);
+                var detailhtmlpanel = item.down("container[xtype=meetinginfo.DetailPanel]");
 
-                }else if (tabItem.itemPKV&&tabItem.itemPKV!=pkValue){
-                    self.Warning("你已经打开一个编辑窗口了");
-                    return;
+                //处理数据字段的值
+/*                var ddItem = factory.DDCache.getItemByDDCode("MEETINGCATEGORY");
+                var resultVal = "";
+                var value = recordData["meetingCategory"];
+                for (var i = 0; i < ddItem.length; i++) {
+                    var ddObj = ddItem[i];
+                    if (value == ddObj["itemCode"]) {
+                        resultVal = ddObj["itemName"];
+                        break;
+                    }
                 }
-                tabPanel.setActiveTab(tabItem);  
+                recordData.meetingCategory = resultVal;*/
+                recordData.needChecking = recordData.needChecking == 1 ? "需要考勤" : "不考勤";
+                detailhtmlpanel.setData(recordData);
+
+            }, 30);
+
+        } else if (tabItem.itemPKV && tabItem.itemPKV != pkValue) {
+            self.Warning("你已经打开一个编辑窗口了");
+            return;
+        }
+        tabPanel.setActiveTab(tabItem);
     },
 });
