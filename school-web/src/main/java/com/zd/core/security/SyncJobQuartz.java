097e2b4558bc71cd3f76578666649ae4998e2692
zzk-huitu <540323699@qq.com>
2017-11-13 15:47:55 +0800

    update

--- school-web/src/main/java/com/zd/core/security/SyncJobQuartz.java
+++ school-web/src/main/java/com/zd/core/security/SyncJobQuartz.java
@@ -1,12 +1,27 @@
 package com.zd.core.security;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.orcl.sync.model.hibernate.hibernate.HrDepartment;
+import com.orcl.sync.model.hibernate.hibernate.HrDeptPosition;
+import com.orcl.sync.model.hibernate.hibernate.HrPosition;
+import com.orcl.sync.model.hibernate.hibernate.HrUser;
+import com.orcl.sync.model.hibernate.hibernate.HrUserDepartmentPosition;
+import com.zd.core.util.CustomerContextHolder;
 import com.zd.core.util.DBContextHolder;
+import com.zd.school.plartform.baseset.model.BaseOrgToUP;
+import com.zd.school.plartform.baseset.service.BaseOrgService;
 import com.zd.school.plartform.system.model.CardUserInfoToUP;
 import com.zd.school.plartform.system.model.SysUserToUP;
 import com.zd.school.plartform.system.service.SysUserService;
@@ -16,13 +31,24 @@ public class SyncJobQuartz {
 
 	@Resource
 	private SysUserService thisService; // service层接口
-
+	
+	@Resource
+    private SessionFactory sssssss;
+    @Resource
+    private BaseOrgService orgService;
+    
 	private static Logger logger = Logger.getLogger(SyncJobQuartz.class);
 
 	protected void execute() {
 		try {
 			int row=0;
-			// 1：同步OA用户、部门数据
+//			// 1：同步OA用户、部门数据
+//			row=syncOaUserAndDept();
+//			if(row>=0){
+//				logger.info("定时同步OA人员部门数据成功！");
+//			}else{
+//				logger.info("定时同步OA人员部门数据失败，详见错误日志！");
+//			}
 			
 			// 2：同步人员数据到UP
 			row=SyncUserIntoUp();
@@ -31,6 +57,7 @@ public class SyncJobQuartz {
 			}else{
 				logger.info("定时同步人员数据失败，详见错误日志！");
 			}
+			
 			// 3：同步UP中的发卡数据
 			row=SyncUserCardFromUp();
 			if(row>=0){
@@ -142,4 +169,50 @@ public class SyncJobQuartz {
 
 		return row;
 	}
+	
+    private int syncOaUserAndDept() {
+    	int row = 0;
+        try {
+        	 //启用orcl数据库
+	        CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_ORACLE);
+	        Session session = sssssss.openSession();
+	        session.beginTransaction();
+	        
+	        //1:查询OA的部门数据
+	        Query query = session.createQuery("from HrDepartment");
+	        List<HrDepartment> deptList = query.list();
+	        
+	        //2:查询OA的岗位数据
+	        query = session.createQuery("from HrPosition");
+	        List<HrPosition> jobList = query.list();
+	        
+	        //3:查询OA的部门岗位数据
+	        query = session.createQuery("from HrDeptPosition");
+	        List<HrDeptPosition> deptJobList = query.list();
+	        
+	        query = session.createQuery("from HrUserDepartmentPosition where departmentId is not null and deptPositionId is not null");
+	        List<HrUserDepartmentPosition> userDeptList = query.list();
+	        
+	        //4：查询用户数据
+            query = session.createQuery("from HrUser where accounts is not null ");
+            List<HrUser> userList = query.list();
+
+	        //提交事务
+	        session.getTransaction().commit();
+	        //关闭session
+	        session.flush();
+	        session.close();
+	        
+	        //切换回Q1
+	        CustomerContextHolder.setCustomerType(CustomerContextHolder.SESSION_FACTORY_MYSQL);
+	        
+	        Integer state=thisService.doSyncOaUserandDept(deptList,jobList,deptJobList,userDeptList,userList);
+	        
+	        row=1;
+        } catch (Exception e) {
+        	row = -1;
+            logger.error(e.getMessage());         
+        }
+        return row;
+    }
 }
